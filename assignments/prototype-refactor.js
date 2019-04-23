77752a41c6a16dcf833886e5e9ea2a04c9bf62d5
Ray Thurman <raymondthurman5@gmail.com>
2019-04-23 12:11:48 -0400

    refactor updates

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,77 +7,75 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
-class GameObject {
-    constructor(attr) {
-        this.createdAt = attr.createdAt;
-        this.name = attr.name;
-        this.dimensions = attr.dimensions;
-    }
-    destroy() {
-        return `${this.name} was removed from the game.`;
-    }
-}
 
+// Game object
+class GameObject {
+  constructor(attr) {
+    this.createdAt = attr.createdAt;
+    this.name = attr.name;
+    this.dimensions = attr.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
 
 /*      Character      */
 class CharacterStats extends GameObject {
-    constructor(charAttr) {
-        super(charAttr);
-        this.healthPoints = charAttr.healthPoints;
+  constructor(charAttr) {
+    super(charAttr);
+    this.healthPoints = charAttr.healthPoints;
+  }
+  takeDamage(i) {
+    this.healthPoints = this.healthPoints - i;
+    if (this.healthPoints > 0) {
+      return `${this.name} took ${i} damage.\nHealth Points: ${
+        this.healthPoints
+      }`;
+    } else if (this.healthPoints <= 0) {
+      return this.destroy();
     }
-    takeDamage(i) {
-        this.healthPoints = this.healthPoints - i;
-        if (this.healthPoints > 0) {
-            return `${this.name} took ${i} damage.\nHealth Points: ${this.healthPoints}`;
-        } else if (this.healthPoints <= 0) {
-            return this.destroy();
-        }
-    }
+  }
 }
-
 
 /*      Humans      */
 class Humanoid extends CharacterStats {
-    constructor(humanAttr) {
-        super(humanAttr);
-        this.team = humanAttr.team;
-        this.weapons = humanAttr.weapons;
-        this.language = humanAttr.language;
-    }
-    greet() {
-        return `${this.name} offers a greeting in ${this.language}`;
-    }
+  constructor(humanAttr) {
+    super(humanAttr);
+    this.team = humanAttr.team;
+    this.weapons = humanAttr.weapons;
+    this.language = humanAttr.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
 }
-
-
 
 /*      Hero      */
 
 class Hero extends Humanoid {
-    constructor(heroAttr) {
-        super(heroAttr);
-        this.armor = heroAttr.armor;
-    }
-    heroicSlash(target) {
-        let i = Math.floor(Math.random() * 6);
-        return target.takeDamage(i);
-    }
+  constructor(heroAttr) {
+    super(heroAttr);
+    this.armor = heroAttr.armor;
+  }
+  heroicSlash(target) {
+    let i = Math.floor(Math.random() * 6);
+    return target.takeDamage(i);
+  }
 }
-
 
 /*      Villain     */
 
 class Villain extends Humanoid {
-    constructor(villainAttr) {
-        super(villainAttr);
-        this.magic = villainAttr.magic;
-    }
-    frostbolt(target) {
-        let i = Math.floor(Math.random() * 3);
-        return target.takeDamage(i);
-    }
+  constructor(villainAttr) {
+    super(villainAttr);
+    this.magic = villainAttr.magic;
+  }
+  frostbolt(target) {
+    let i = Math.floor(Math.random() * 3);
+    return target.takeDamage(i);
+  }
 }
-
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -87,91 +85,76 @@ class Villain extends Humanoid {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-
 const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-        length: 2,
-        width: 1,
-        height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-        'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue",
 });
 
 const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-        length: 2,
-        width: 2,
-        height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-        'Giant Sword',
-        'Shield',
-    ],
-    language: 'Common Tongue',
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue",
 });
 
 const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-        length: 1,
-        width: 2,
-        height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-        'Bow',
-        'Dagger',
-    ],
-    language: 'Elvish',
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish",
 });
 
 const mainHero = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-        length: 2,
-        width: 3,
-        height: 4,
-    },
-    healthPoints: 20,
-    name: 'Thor',
-    team: 'Odin',
-    weapons: [
-        'StormBreaker',
-        'Lightning',
-    ],
-    language: 'God',
-    armor: 5,
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 3,
+    height: 4,
+  },
+  healthPoints: 20,
+  name: "Thor",
+  team: "Odin",
+  weapons: ["StormBreaker", "Lightning"],
+  language: "God",
+  armor: 5,
 });
 
 const mainVillain = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-        length: 2,
-        width: 3,
-        height: 4,
-    },
-    healthPoints: 20,
-    name: 'Loki',
-    team: 'Odin',
-    weapons: [
-        'Dagger',
-        'Magic',
-    ],
-    language: 'God',
-    magic: 15,
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 3,
+    height: 4,
+  },
+  healthPoints: 20,
+  name: "Loki",
+  team: "Odin",
+  weapons: ["Dagger", "Magic"],
+  language: "God",
+  magic: 15,
 });
 
 console.log(mage.createdAt); // Today's date
